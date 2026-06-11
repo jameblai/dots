89411616cb1dbb55b99a38465e312673b0705399
James Blair <james@blair.nz>
2026-06-11 20:00:29 +1200

    feat: vscode user settings

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -4,5 +4,11 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
+    userSettings = {
+      "editor.fontFamily" = "TX-02, 'Symbols Nerd Font'";
+      "editor.fontSize" = 18;
+      "terminal.integrated.fontFamily" = "TX-02, 'Symbols Nerd Font'";
+      "terminal.integrated.fontSize" = 15;
+    };
   };
 }
